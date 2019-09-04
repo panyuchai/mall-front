f466a587571ff336e182c065d908a444327c88d0
潘玉钗 <1156412238@qq.com>
2019-09-04 10:37:07 +0800

    init

---
+++ pages/TransferPage/TransferPage.vue
@@ -0,0 +1,26 @@
+<template>
+	<view>
+		{{initData}}
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				initData: ''
+			}
+		},
+		methods: {
+			
+		},
+		onLoad(options){
+			console.log(options);
+			this.initData=options
+		}
+	}
+</script>
+
+<style>
+
+</style>
