fd2956dc618cda76062aa2c0894610980dc7e55e
潘玉钗 <1156412238@qq.com>
2019-09-16 09:23:34 +0800

    1

--- pages/payment/payment.vue
+++ pages/payment/payment.vue
@@ -368,7 +368,7 @@
 						}, 1000)
 					}else{
 						uni.setStorageSync('PAYMENT_ORDER_INFO', JSON.stringify(data));
-						window.location.href=`http://testpay.yujianli.cn/#/pages/payment/cashRegister?orderNo=`+res.result.orderNo
+						window.location.href=`http://testpay.yujianli.cn/#/pages/payment/cashRegister?orderNo=`+res.result.orderNo+'&mallDomain=yyy'
 						// uni.navigateTo({
 						//     url: 'http://testpay.yujianli.cn/#/pages/payment/cashRegister?orderNo='+res.result.orderNo
 						// });
