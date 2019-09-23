de49346aea3a838c91c1779db3bb3344de970b54
潘玉钗 <1156412238@qq.com>
2019-09-23 19:17:04 +0800

    1

--- pages/payment/cashRegister.vue
+++ pages/payment/cashRegister.vue
@@ -201,7 +201,10 @@
 					if(res.err_msg == "get_brand_wcpay_request:ok" ){
 					  // 使用以上方式判断前端返回,微信团队郑重提示：
 					  //res.err_msg将在用户支付成功后返回ok，但并不保证它绝对可靠。
-					  console.log('res.err_msg == "get_brand_wcpay_request:ok"')
+					  alert('res.err_msg == "get_brand_wcpay_request:ok"');
+					}else{
+						alert('支付失败-----------------');
+						window.location.href=this.transferUrl + '?mallDomain='+this.mallDomain;
 					}
 				  }
 				)
@@ -320,7 +323,7 @@
 			}
 		},
 		computed: {
-			...mapState('common', ['baseInfo', 'userInfo']),
+			...mapState('common', ['baseInfo', 'transferUrl']),
 		},
 		onLoad(options){
 			uni.setStorageSync('ORDER_NO', options.orderNo);
