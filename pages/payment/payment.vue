af9fd29d497140145d14d844d5b26ecbaaa33ac0
潘玉钗 <1156412238@qq.com>
2019-10-09 17:35:44 +0800

    1

--- pages/payment/payment.vue
+++ pages/payment/payment.vue
@@ -328,6 +328,7 @@
 						}, 1000)
 					}else{
 						if(res.code == 0){
+							alert(this.paymentUrl);
 							window.location.href=`http://${this.paymentUrl}.yujianli.cn/#/pages/payment/cashRegister?orderNo=`+res.result.orderNo+"&mallDomain="+this.baseInfo.mallDomain+'&orderPayPrice='+data.payPrice+'&baseUrl='+this.baseUrl+'&token='+encodeURIComponent(this.token);
 						}else{
 							uni.showToast({
