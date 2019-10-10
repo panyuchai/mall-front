d969368583f7c9641c690316af4358cc4977ddb3
潘玉钗 <1156412238@qq.com>
2019-10-10 17:01:37 +0800

    1

--- pages/payment/cashRegister.vue
+++ pages/payment/cashRegister.vue
@@ -190,12 +190,10 @@
 					  // 使用以上方式判断前端返回,微信团队郑重提示：
 					  //res.err_msg将在用户支付成功后返回ok，但并不保证它绝对可靠。
 					  // uni.removeStorageSync('chooseAddress');
-					  alert(this.mallDomain);
-					    window.location.href='//'+that.baseUrl + '#/pages/orderDetail/orderDetail?orderId='+that.initData.orderId+"&mallDomain="+this.mallDomain;
+					    window.location.href='//'+that.baseUrl + '#/pages/orderDetail/orderDetail?orderId='+that.initData.orderId+"&mallDomain="+that.mallDomain;
 					}else{
 						// uni.removeStorageSync('chooseAddress');
-						alert(this.mallDomain);
-						window.location.href='//'+that.baseUrl + '#/pages/order/order?state=0'+"&mallDomain="+this.mallDomain;
+						window.location.href='//'+that.baseUrl + '#/pages/order/order?state=0'+"&mallDomain="+that.mallDomain;
 					}
 				  }
 				)
